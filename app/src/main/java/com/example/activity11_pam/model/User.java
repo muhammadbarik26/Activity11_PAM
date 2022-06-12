f6dceaf696c1888841d0567bff635149475dc65a
muhammadbarik26 <mhdbarik26@gmail.com>
2022-06-12 21:09:38 +0700

    Activity11_20200140078

---
+++ app/src/main/java/com/example/activity11_pam/model/User.java
@@ -0,0 +1,38 @@
+package com.example.activity11_pam.model;
+
+public class User {
+    private String id, name, email;
+
+    public User() {
+
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
